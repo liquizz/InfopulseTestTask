58121386d4ee21e6d4c9c5b3d75ba6bf35c8dd63
Vladislav Sheyko <vladislav.sheyko@hotmail.com>
2021-02-03 17:02:55 +0200

    Created Basic angular app

---
+++ WebApp/my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrdersComponent,
+    ProductsComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
